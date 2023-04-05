f07d62edb12cf5966fd0af3cac322f4bd93eafa1
Siddiquenahian <nahian198@gmail.com>
2023-04-05 16:42:10 +0600

    product added

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Shop.css';
+import Product from '../Product/Product';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -13,7 +14,12 @@ const Shop = () => {
     return (
         <div className='shop-container'>
             <div className="products-container">
-                <h2>Products Coming Here: {products.length}</h2>
+                {
+                    products.map(product => <Product
+                    key={product.id}
+                    product = {product}
+                    ></Product>)
+                }
             </div>
             <div className="cart-container">
                 <h3>Order Summary</h3>
