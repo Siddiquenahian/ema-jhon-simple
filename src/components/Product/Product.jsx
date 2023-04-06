2379798538bfaab630ca300875e08f07750db2f2
Siddiquenahian <nahian198@gmail.com>
2023-04-06 14:02:48 +0600

    set event handler added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-    // console.log(props.product);
+    // console.log(props);
     const {img, name, seller, ratings, price} = props.product;
+
+    const handleAddToCart = props.handleAddToCart;
+    
     return (
       <div className="product">
         <img src={img} alt="" />
-        <div className='product-info'>
+        <div className="product-info">
           <h6 className="product-name">{name}</h6>
           <p>Price: ${price}</p>
           <p>Manufacturer: {seller}</p>
           <p>Ratings: {ratings}</p>
         </div>
-        <button className='btn-cart'>Add to Cart</button>
+        <button onClick={ () => handleAddToCart(props.product) } className="btn-cart">
+          Add to Cart
+        </button>
       </div>
     );
 };
