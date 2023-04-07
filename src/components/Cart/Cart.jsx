1ca57f770da40a6dc45dbc4c42ae739d49519a39
Siddiquenahian <nahian198@gmail.com>
2023-04-07 21:57:45 +0600

    added car components and calculation

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Cart.css';
+const Cart = (props) => {
+    const cart = props.cart; //option-1
+    //const {cart} = props; option-2
+
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax = total*7/100;
+    const grandTotal = total + shipping + tax;
+  return (
+    <div className='cart'>
+      <h3>Order Summary</h3>
+      <p>Selected Items: {cart.length}</p>
+      <p>Total Price: ${total}</p>
+      <p>Shipping: ${shipping}</p>
+      <p>Tax: ${tax.toFixed(2)}</p>
+      <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
+    </div>
+  );
+};
+import './Cart.css';
+export default Cart;
