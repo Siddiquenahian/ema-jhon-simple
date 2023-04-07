162671235a21b7ea3a0022e115afdbc8deeddd32
Siddiquenahian <nahian198@gmail.com>
2023-04-07 23:13:09 +0600

    Handle Quantity

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,16 +6,19 @@ const Cart = (props) => {
 
     let total = 0;
     let shipping = 0;
+    let quantity = 0;
     for (const product of cart){
-        total = total + product.price;
+        product.quantity = product.quantity || 1;
+        total = total + product.price * product.quantity;
         shipping = shipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = total*7/100;
     const grandTotal = total + shipping + tax;
   return (
     <div className='cart'>
       <h3>Order Summary</h3>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Price: ${total}</p>
       <p>Shipping: ${shipping}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
